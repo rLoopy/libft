3473b54ba1952f04a2307e29b194baa028a6ad99
Rayan Rohrer <rrohrer@c2r2s4.42lausanne.ch>
2025-10-01 18:17:14 +0200

    second day

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 16:42:47 by loopy             #+#    #+#             */
-/*   Updated: 2025/10/01 17:21:55 by rrohrer          ###   ########.fr       */
+/*   Updated: 2025/10/01 18:16:55 by rrohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	size_t i;
+	char *a = (char *)s1;
+	char *b = (char *)s2;
 	
+	while (n && a[i] && b[i] && a[i] == b[i])
+	{
+		i++;
+		n--;
+	}
+	if (n == 0)
+		return (0);
+	return (a[i] - b[i]);
 }
