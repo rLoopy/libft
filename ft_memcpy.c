66c624df6115e763cb8c083ca073279d77a3fa70
Loopy <rayanrohrer@Macbook-de-Loopy.local>
2025-10-01 18:42:20 +0200

    second day

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -16,10 +16,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!dest && !src)
+		return (NULL);
 	i = 0;
 	while (n)
 	{
-		((char *)dest)[i] = ((char *)src)[i];
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 		i++;
 		n--;
 	}
