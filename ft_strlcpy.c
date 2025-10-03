d8a08d3d063aa353f05c2e2fc02d3bcfa4d1a6c5
Rayan Rohrer <rrohrer@c2r3s3.42lausanne.ch>
2025-10-03 16:56:26 +0200

    4th day

---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 16:18:16 by rrohrer           #+#    #+#             */
+/*   Updated: 2025/10/03 16:56:07 by rrohrer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+ {
+	size_t	i;
+	size_t max;
+
+	max = size -1;
+	i = 0;
+	while (src[i] && max)
+	{
+		dest[i] = src[i];
+		i++;
+		max--;
+	}
+	return (i);
+	dest[i] = '\0';
+ }
+ 
