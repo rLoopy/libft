41ba576854ddae35e99e0e814220f88dad169a5b
Rayan Rohrer <rrohrer@c2r3s3.42lausanne.ch>
2025-10-03 15:54:02 +0200

    4th day

---
+++ ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 15:21:07 by rrohrer           #+#    #+#             */
+/*   Updated: 2025/10/03 15:52:58 by rrohrer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_atoi(const char *nptr)
+{
+	int	res;
+	int signe;
+	int	i;
+
+	i = 0;
+	signe = 1;
+	res = 0;
+	if (!nptr)
+		return 0;
+	if (nptr[0] == '-')
+	{
+		signe = -1;
+		i++;
+	}
+	if (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		res = res * 10;
+		res = res + (nptr[i] - '0');
+		i++;
+	}
+	return (res * signe);
+}
