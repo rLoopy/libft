0cafd9e7d12ee47677ed0f16358f5fa1ef30f918
rLoopy <loopyftn@gmail.com>
2025-10-07 01:27:38 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: loopy <loopy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 15:14:04 by loopy             #+#    #+#             */
-/*   Updated: 2025/10/02 15:14:39 by loopy            ###   ########.fr       */
+/*   Updated: 2025/10/04 21:33:36 by loopy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	int i;
+	char *dest;
+
+	i = 0;
+	dest = malloc(len + 1);
+	if (!dest)
+		return (NULL);
+	while (len)
+	{
+		dest[i] = s[start++];
+		i++;
+		len--;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
