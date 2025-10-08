f482d6325df9efabc0b483ab55ab4208369e5867
Rayan Rohrer <rrohrer@c2r2s6.42lausanne.ch>
2025-10-08 14:25:39 +0200

    done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 18:18:18 by rrohrer           #+#    #+#             */
-/*   Updated: 2025/10/03 18:19:20 by rrohrer          ###   ########.fr       */
+/*   Updated: 2025/10/08 14:25:28 by rrohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	i;
+	size_t	j;
+	size_t	src_len;
+
+	i = ft_strlen(dst);
+	while (size - 1 > i)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return;
 	
 }
