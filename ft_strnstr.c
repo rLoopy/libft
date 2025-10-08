db6ad2ade73f96f186f8d44df135881732a170bb
Rayan Rohrer <rrohrer@c2r2s6.42lausanne.ch>
2025-10-08 18:19:38 +0200

    r

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,19 +6,20 @@
 /*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 14:35:59 by rrohrer           #+#    #+#             */
-/*   Updated: 2025/10/08 15:38:26 by rrohrer          ###   ########.fr       */
+/*   Updated: 2025/10/08 18:19:24 by rrohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *strnstr(const char *big, const char *little, size_t len)
+char *ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t i;
 
 	i = 0;
 	
-	if (!little)
-		return (big);
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (big[i] );
 	
 }
