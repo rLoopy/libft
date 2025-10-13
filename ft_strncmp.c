824e1a65e950783f826ab790ee3d490bc07a58b1
rLoopy <loopyftn@gmail.com>
2025-10-13 15:20:30 +0200

    strncmp corrected

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
+/*   By: loopy <loopy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 06:23:12 by loopy             #+#    #+#             */
-/*   Updated: 2025/10/10 17:34:21 by rrohrer          ###   ########.fr       */
+/*   Updated: 2025/10/13 15:18:57 by loopy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	if (n == 0)
 		return (0);
-	return (s1[i] - s2[i]);
+	return (((unsigned char)s1[i] - (unsigned char)s2[i]));
 }
