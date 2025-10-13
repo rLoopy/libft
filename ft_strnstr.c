753ff365a418453d7083e752c865f61a3764cf1b
rLoopy <loopyftn@gmail.com>
2025-10-13 17:18:23 +0200

    strnstr corrected

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
+/*   By: loopy <loopy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 14:35:59 by rrohrer           #+#    #+#             */
-/*   Updated: 2025/10/10 17:34:33 by rrohrer          ###   ########.fr       */
+/*   Updated: 2025/10/13 17:18:10 by loopy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,15 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	i = 0;
 	if (little[0] == '\0')
-		return ((char *)&big[i]);
-	if (ft_strlen(little) > len)
-		return (NULL);
-	while (len && big[i])
+		return ((char *)big);
+	while (i < len && big[i])
 	{
 		j = 0;
-		while (little[j] && big[i + j] == little[j])
-		{
+		while (i + j < len && little[j] && big[i + j] == little[j])
 			j++;
-		}
 		if (little[j] == '\0')
 			return ((char *)&big[i]);
 		i++;
-		len--;
 	}
 	return (NULL);
 }
