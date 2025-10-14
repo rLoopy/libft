2618d93411749293414713474219579a7af3d8ed
rLoopy <loopyftn@gmail.com>
2025-10-14 17:36:03 +0200

    itoa done

---
+++ ft_itoa.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loopy <loopy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 18:51:09 by loopy             #+#    #+#             */
+/*   Updated: 2025/10/14 17:29:36 by loopy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_count_digits(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n <= 0)
+		count = 1;
+	while (n)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len = ft_count_digits(n);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	if (n == 0)
+		str[0] = '0';
+	i = len - 1;
+	while (n)
+	{
+		str[i--] = (n % 10) + '0';
+		n /= 10;
+	}
+	str[len] = '\0';
+	return (str);
+}
