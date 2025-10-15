36a238aefb136761d23d7bb85f644f5240c18f11
Loopy <rayanrohrer@Macbook-de-Loopy.local>
2025-10-15 14:27:41 +0200

    windows

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rrohrer <rrohrer@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rayanrohrer <rayanrohrer@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 18:57:39 by loopy             #+#    #+#             */
-/*   Updated: 2025/10/10 17:34:42 by rrohrer          ###   ########.fr       */
+/*   Updated: 2025/10/15 14:17:41 by rayanrohrer      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	end = ft_strlen(s1);
+	// TODO: Trouver le début (start) - avancer tant que s1[start] est dans set
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	// TODO: Trouver la fin (end) - reculer tant que s1[end - 1] est dans set
+	while (/* condition à compléter */)
+		end--;
+	// TODO: Utiliser ft_substr pour extraire de start à (end - start)
+	return (/* à compléter */);
 }
