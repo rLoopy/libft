1512198add5579726cfd7818f9fad8d160dd68df
rayanrohrer <rayanrohrer@student.42.fr>
2025-10-16 18:12:26 +0200

    Add ft_split implementation and .gitignore

---
+++ ft_split.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rayanrohrer <rayanrohrer@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 15:56:00 by rayanrohrer       #+#    #+#             */
+/*   Updated: 2025/10/15 17:40:56 by rayanrohrer      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	count_words(char const *s, char c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+static char	*copy_word(char const *s, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	word = malloc(end - start + 1);
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (start < end)
+	{
+		word[i] = s[start];
+		i++;
+		start++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static void	free_all(char **result, int j)
+{
+	int	i;
+
+	i = 0;
+	while (i < j)
+	{
+		free(result[i]);
+		i++;
+	}
+	free(result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+	int		i;
+	int		j;
+	int		start;
+
+	result = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+			result[j] = copy_word(s, start, i);
+		if (i > start && !result[j++])
+			return (free_all(result, j - 1), NULL);
+	}
+	result[j] = NULL;
+	return (result);
+}
